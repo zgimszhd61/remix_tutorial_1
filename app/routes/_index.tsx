45c70ef9c319f0a74f64a81f8c5cc3c8c6c269b7
xiaoming <lininruc@gmail.com>
2024-07-31 20:43:19 +0800

    Add mvs

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
+import { Link, NavLink } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,40 +11,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flax font-sans p-4">
+      <Button>Click me</Button>
+      <Link to="/dashboard">Dashboard</Link>;
+      <NavLink
+      to="/messages"
+      className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "active" : ""
+      }
+    >
+      Messages
+    </NavLink>;
     </div>
   );
 }
---
+++ app/routes/auth/users.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function users() {
+  return (
+    <div>
+      <h1>hello users</h1>
+    </div>
+  )
+}
+
+export default users
